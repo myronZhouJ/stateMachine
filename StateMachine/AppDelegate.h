ea8524ef8cda080d67f7c56b6fa8343c9fdaab71
Myron <540243706@qq.com>
2017-08-24 15:03:21 +0800

    Initial Commit

---
+++ StateMachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StateMachine
+//
+//  Created by Myron on 8/24/17.
+//  Copyright © 2017 Pikicast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StateMachine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StateMachine
+//
+//  Created by Myron on 8/24/17.
+//  Copyright © 2017 Pikicast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
